6eb02073fd82b7149cc82f0b9f188f43d4d7d24d
kasooo <kasoo@126.com>
2021-08-18 12:24:54 +0800

    Update lagou.js

--- lagou.js
+++ lagou.js
@@ -1,16 +1,12 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-
-
-//if (obj.content.contentCardList != undefined) {
-    for (var i = 0; i < obj.content.contentCardList.length; i++) {
-        var card = obj.content.contentCardList[i];
-        for (var j = 0; j < card.courseList.length; j++) {
-            card.courseList[j].hasBuy = true;
-        }
+for (var i = 0; i < obj.content.contentCardList.length; i++) {
+    var card = obj.content.contentCardList[i];
+    for (var j = 0; j < card.courseList.length; j++) {
+        card.courseList[j].hasBuy = true;
     }
-//}
+}
 body = JSON.stringify(obj);
 
 $done(body);
