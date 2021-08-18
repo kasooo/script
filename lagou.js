137a9bbc19d9989e9083fbd47b317cf91bda2d43
kasooo <kasoo@126.com>
2021-08-18 12:25:16 +0800

    Update lagou.js

--- lagou.js
+++ lagou.js
@@ -1,6 +1,5 @@
 var body = $response.body;
 var obj = JSON.parse(body);
-
 for (var i = 0; i < obj.content.contentCardList.length; i++) {
     var card = obj.content.contentCardList[i];
     for (var j = 0; j < card.courseList.length; j++) {
