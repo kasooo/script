61232b0be76d0a764288a47fd48b0e233f0e08ac
kasooo <kasoo@126.com>
2021-08-18 10:11:54 +0800

    Create lagou.js

---
+++ lagou.js
@@ -0,0 +1,13 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+for (var i = 0; i < obj.content.contentCardList.length; i++) {
+    var card = obj.content.contentCardList[i];
+    for (var j = 0; j < card.courseList.length; j++) {
+        card.courseList[j].hasBuy = true;
+    }
+}
+body = JSON.stringify(obj);
+
+console.log(body);
+
+$done(body);
